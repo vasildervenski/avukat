07be3598cf0a4f53e04bef741bdba162c1cdc92f
vasil dervenski <vasil_d@mail.bg>
2023-02-03 13:02:07 +0200

    Another commit

--- xmlrpc.php
+++ xmlrpc.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=/avukat/?simply_static_page=109">
+		<meta http-equiv="refresh" content="0;url=/?simply_static_page=14">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "/avukat/?simply_static_page=109";
+			window.location = "/?simply_static_page=14";
 		</script>
 
-		<p>You are being redirected to <a href="/avukat/?simply_static_page=109">/avukat/?simply_static_page=109</a></p>
+		<p>You are being redirected to <a href="/?simply_static_page=14">/?simply_static_page=14</a></p>
 	</body>
 </html>
