8f590ca33ce3285eed0fbf1991b474cdbd4eaf95
vasil dervenski <vasil_d@mail.bg>
2023-02-03 12:38:26 +0200

    Second commit

---
+++ simply-static-1-1675317727/xmlrpc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=/?simply_static_page=14">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "/?simply_static_page=14";
+		</script>
+
+		<p>You are being redirected to <a href="/?simply_static_page=14">/?simply_static_page=14</a></p>
+	</body>
+</html>
